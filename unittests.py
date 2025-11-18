eff68545b81d771d8b33b67e1d56d42672f52911
Masonk7 <150885585+Masonk7@users.noreply.github.com>
2025-11-18 13:42:23 -0600

    unittests for symbol

--- unittests.py
+++ unittests.py
@@ -8,3 +8,12 @@ from unittests import(
     validate_date,
 )
 
+def test_symbol_valid(self):
+    self.assertTrue(validate_symbol("AAPL"))
+    self.assertTrue(validate_symbol("A"))
+    self.AssertTrue(validate_symbol("ABCDEFG"))
+def test_symbol_invalid(self):
+    self.assertFalse(validate_symbol("ABCDEFGH"))
+    self.assertFalse(validate_symbol("aapl"))
+    self.assertFalse(validate_symbol("AAP1"))
+
