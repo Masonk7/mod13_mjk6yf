4313f8363ea9c04de50adfe344bed807f0c897af
Masonk7 <150885585+Masonk7@users.noreply.github.com>
2025-11-18 13:52:51 -0600

    unittests for chart type

--- unittests.py
+++ unittests.py
@@ -17,3 +17,12 @@ def test_symbol_invalid(self):
     self.assertFalse(validate_symbol("aapl"))
     self.assertFalse(validate_symbol("AAP1"))
 
+def test_chart_type_valid(self):
+    self.assertTrue(validate_chart_type("1"))
+    self.assertTrue(validate_chart_type("2"))
+def test_chart_type_invalid(self):
+    self.assertFalse(validate_chart_type("0"))
+    self.assertFalse(validate_chart_type("3"))
+    self.assertFalse(validate_chart_type("a"))
+    self.assertFalse(validate_chart_type("12"))
+
